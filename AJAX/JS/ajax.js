37b2cf34af4c35d70e903c6f6024f375a2e62077
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2021-05-13 16:12:21 -0700

    Made changes to AJAX folder
    Added new Pikachu.jpg and additional effect to whosThatPokemon()

--- AJAX/JS/ajax.js
+++ AJAX/JS/ajax.js
@@ -3,6 +3,7 @@ function whosThatPokemon() {
     xhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             document.getElementById("who").innerHTML = this.responseText;
+            document.getElementById("pic").src = "img/Pikachu.jpg";
         }
     };
     xhttp.open("Get", "JS/server.txt", true);
